71cfaebdffe427d7ca7343558632ab86c17d47c9
Imran chohan <imranhassan03333@gmail.com>
2024-12-28 10:00:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let username = "imran._.chohan";
+let titled = "Hello World";
+let myname = "Imran";
+let age = "18 years old";
+let passion = "Certified Fronend Web Developer"
+let email = "My Email address is  ::  imranhassan03333@gmail.com"
+let book = "A smarter way to learn JavaScript"
+let str = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(titled);
+alert(myname);
+alert(age);
+alert(passion);
+alert(email);
+alert(book);
+alert(str);
+document.write("yay! I can write HTML content through JavaScript");
+document.write("Rules for naming JS variables");
+document.write("Variables names can only contain, numbers, $ and _ . For example : $my_1stVariable");
+document.write("Variables must begin with a letter, $ or _ . For example : $name, _ name or name");
+document.write("Variables names are case sensitive");
+document.write("Variables names should not be js keywords");
+
+
+
